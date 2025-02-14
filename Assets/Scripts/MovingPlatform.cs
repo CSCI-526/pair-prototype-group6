c82615eaa3754976c5e1e8091b02332e86436de5
Apurva Patil <58740201+apurvaapatil@users.noreply.github.com>
2025-02-14 15:35:24 -0800

    updated platforms and detect collision

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,42 +4,41 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    public float movementDistance;
     public float speed;
-    public int startPoint;
-    public Transform[] points;  // points where platform needs to move
 
-    private int i;  // index of array
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        // setting the position of platform to the position of one of the points using index "startIndex"
-        transform.position = points[startPoint].position;
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
+        movingLeft = true;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // check the distance between platform and point
-        if(Vector2.Distance(transform.position, points[i].position) < 10f){
-            i++;
-            // check if platform on last point
-            if(i == points.Length){
-                i = 0;
+        if(movingLeft){
+            if(transform.position.x > leftEdge){
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else{
+                movingLeft = false;
             }
         }
-
-        // moving the platform to ith position
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
-    } 
-
-    private void OnCollisionEnter2D(Collision2D collisionInfo)
-    {
-        collisionInfo.transform.SetParent(transform);
+        else{
+            if(transform.position.x < rightEdge){
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else{
+                movingLeft = true;
+            }
+        }
     }
-
-    // private void OnCollisionExit2D(Collision2D collisionInfo)
-    // {
-    //     collisionInfo.transform.SetParent(null);
-    // }
 }
